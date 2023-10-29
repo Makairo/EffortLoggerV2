a17efe4aa1eb5290e5a1d5cb8184e79152710fb8
Andrew Hejl <77745556+Makairo@users.noreply.github.com>
2023-10-28 20:55:02 -0700

    Update

--- src/application/riskReductionHandling.java
+++ src/application/riskReductionHandling.java
@@ -9,6 +9,20 @@ public class riskReductionHandling
 	//Restricting Dangerous File Upload: Gavin
 	
 	//Ensuring and Requiring Authentication: Andrew
+	public static int RequestRank(User inUser)
+	{
+		return inUser.getRank();
+	}
+	
+	public static int RequestId(User inUser)
+	{
+		return inUser.getID();
+	}
+	
+	public static Boolean AssertRank(User inUser, int rank) 
+	{
+		return inUser.getRank() == rank;
+	}
 	
 	//Ensuring Anonymity: Kyle
 }
